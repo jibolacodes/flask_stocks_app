7c0925a88ca5dd63b8ba8f64bc514db76c09dba3
jibzsupreme@gmail.com <jibzsupreme@gmail.com>
2022-07-01 17:30:20 +0100

    updated

--- app.py
+++ app.py
@@ -13,19 +13,19 @@ db = SQLAlchemy(app)
 
 # Designing the database
 class BlogPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(20), nullable=False, default='N/A')
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  content = db.Column(db.Text, nullable=False)
+  author = db.Column(db.String(20), nullable=False, default='N/A')
+  date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    def __repr__(self):
-        return 'Blog Post' + str(self.id)
+  def __repr__(self):
+    return 'Blog Post' + str(self.id)
 
 # Using templates for the UI
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 # Passing data to webpages
 @app.route('/posts', methods=['GET', 'POST'])
@@ -33,41 +33,44 @@ def posts():
 
 #CRUD
 # a. Create and Read
-    if request.method == 'POST':
-        post_title = request.form['title']
-        post_author = request.form['author']
-        post_content = request.form['content']
-        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        all_posts = BlogPost.query.all()
-        return render_template('posts.html', posts=all_posts)
+  if request.method == 'POST':
+    post_title = request.form['title']
+    post_author = request.form['author']
+    post_content = request.form['content']
+    new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+    # add new_post to the database
+    db.session.add(new_post)
+    # commit to the database
+    db.session.commit()
+    # redirect to same page which shows the new_post
+    return redirect('/posts')
+  else:
+    # getting the items of BlogPost
+    all_posts = BlogPost.query.all()
+    return render_template('posts.html', posts=all_posts)
 
 # b. Edit
 @app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
-
-    post = BlogPost.query.get_or_404(id)
-    
-    if request.method == 'POST':
-        post.title = request.form['title']
-        post.author = request.form['author']
-        post.content = request.form['content']
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        return render_template('edit.html', post=post)
+  post = BlogPost.query.get_or_404(id)
+  
+  if request.method == 'POST':
+    post.title = request.form['title']
+    post.author = request.form['author']
+    post.content = request.form['content']
+    db.session.commit()
+    return redirect('/posts')
+  else:
+      return render_template('edit.html', post=post)
 
 # c. Delete
 @app.route('/posts/delete/<int:id>')
 def delete(id):
-    post = BlogPost.query.get_or_404(id)
-    db.session.delete(post)
-    db.session.commit()
-    return redirect('/posts')
+  post = BlogPost.query.get_or_404(id)
+  db.session.delete(post)
+  db.session.commit()
+  return redirect('/posts')
 
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+  app.run(debug=True) 
