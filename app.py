7d94aff01850423b493d26b46a129c08a24c17af
jibolacodes <jibzsupreme@gmail.com>
2022-07-05 12:01:28 +0100

    styled pages

--- app.py
+++ app.py
@@ -28,27 +28,20 @@ def index():
   return render_template('index.html')
 
 # Passing data to webpages
+# ===============================================
+#                 #CRUD
+# ===============================================
+# a.Read
 @app.route('/posts', methods=['GET', 'POST'])
 def posts():
 
-#CRUD
-# a. Create and Read
   if request.method == 'POST':
-    post_title = request.form['title']
-    post_author = request.form['author']
-    post_content = request.form['content']
-    new_post = BlogPost(title=post_title, content=post_content, author=post_author)
-    # add new_post to the database
-    db.session.add(new_post)
-    # commit to the database
-    db.session.commit()
-    # redirect to same page which shows the new_post
     return redirect('/posts')
   else:
     # getting the items of BlogPost
     all_posts = BlogPost.query.all()
     return render_template('posts.html', posts=all_posts)
-
+    
 # b. Edit
 @app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
@@ -70,6 +63,23 @@ def delete(id):
   db.session.delete(post)
   db.session.commit()
   return redirect('/posts')
+
+@app.route('/posts/write', methods=['GET', 'POST'])
+def write():
+  if request.method == 'POST':
+    post_title = request.form['title']
+    post_author = request.form['author']
+    post_content = request.form['content']
+    new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+    # add new_post to the database
+    db.session.add(new_post)
+    # commit to the database
+    db.session.commit()
+    # redirect to same page which shows the new_post
+    return redirect('/posts')
+  else:
+      return render_template('write.html')
+
 
 
 if __name__ == "__main__":
---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+const navItem = document.querySelectorAll('.nav-item');
+
+navItem.forEach(item => {
+    console.log(item);
+    item.addEventListener('click', (e) => { 
+        
+    });
+});
+
+
